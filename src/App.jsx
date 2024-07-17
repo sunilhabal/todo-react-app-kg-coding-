148d512d9a9762db846b64a170e00a4b4fbf43c5
sunilhabal <habalsunil@gmail.com>
2024-07-17 18:29:53 +0530

    props update in todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import Todoname from "./components/Todoname";
 import Inbox from "./components/Inbox";
 import Item1 from "./components/Item1";
-import Items2 from "./components/Items2";
 import './App.css'
 
 function App() {
+
   return (
     <center className="todo container">
       <Todoname />
       <Inbox />
 
       <div className="items-container">
-        <Item1 />
-        <Items2 />       
+        <Item1 name1="buy milk" date1={"16/07/2024"} />     
+        <Item1 name2="go to college" date2={"16/07/2024"}/>     
       </div>
 
     </center>
--- src/components/Item1.jsx
+++ src/components/Item1.jsx
@@ -1,10 +1,10 @@
-function Item1() {
+function Item1(p) {
   return (
     <div>
       <div className="items-container">
         <div className="row sb-row">
-          <div className="col-6">Buy Milk</div>
-          <div className="col-4">16/07/2024</div>
+          <div className="col-6">{p.name1} {p.name2}</div>
+          <div className="col-4"> {p.date1} {p.date2}</div>
           <div className="col-2">
             <button type="button" className="btn btn-danger sb-btn">
               DELETE
